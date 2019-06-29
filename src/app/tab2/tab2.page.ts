cc2bf047d78450ae1ddb6bb84a59314c21e8ca8f
Ricardo Espinoza <ricardoespinoza@Ricardos-MacBook-Pro.local>
2019-06-29 18:10:20 -0500

    Mapa Funcional

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,9 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapOptions,
+  Environment
+} from '@ionic-native/google-maps';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +12,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  map: GoogleMap;
+  constructor() {
+    this.loadMap();
+  }
 
-  constructor() {}
+  loadMap(){
+
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+  }
 
 }
